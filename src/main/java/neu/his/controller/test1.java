e8422fa9a17d588dee5d6d40ee24341f8fbcd76f
guanguangchewai <l9426494426@163.com>
2019-06-06 21:02:11 +0800

    1

--- src/main/java/neu/his/controller/test1.java
+++ src/main/java/neu/his/controller/test1.java
@@ -17,16 +17,11 @@ public class test1 {
     UserService userService;
 
     @RequestMapping("list")
-    public void m1(){
+    public void m1() {
         List<User> list = userService.findAll();
-        for(User user : list){
+        for (User user : list) {
             System.out.println(user);
         }
-    }
-
-    @RequestMapping("index")
-    public String m2(){
-        return "index";
     }
 
 }
