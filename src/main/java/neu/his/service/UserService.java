5470b97d3588e3ba32d09da486f4b2e50c64fc3a
lsy7777 <970850441@qq.com>
2019-06-06 22:15:32 +0800

    1

--- src/main/java/neu/his/service/UserService.java
+++ src/main/java/neu/his/service/UserService.java
@@ -11,8 +11,8 @@ public interface UserService {
     //删除
     void deletebyID(int id);
 
-    //查询
-    List<User> findbyID(int id);
+    //查询  attribute_name为属性名 ， attribute为属性内容
+    List<User> findbyattribute_name(String attribute_name,String attribute);
 
     //新增
     void insertuser(User user);
