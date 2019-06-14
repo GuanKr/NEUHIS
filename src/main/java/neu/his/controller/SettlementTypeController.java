dd3a38ea4d28a39911d03e485ca0fcbd5cbc5c54
guanguangchewai <l9426494426@163.com>
2019-06-14 18:41:05 +0800

    1

--- src/main/java/neu/his/controller/SettlementTypeController.java
+++ src/main/java/neu/his/controller/SettlementTypeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 @Controller
-@RequestMapping("SettlementType")
+@RequestMapping("settlementType")
 public class SettlementTypeController {
     @Autowired
     SettlementTypeService settlementTypeService;
