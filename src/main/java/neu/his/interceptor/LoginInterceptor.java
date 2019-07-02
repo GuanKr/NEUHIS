6289811656763c4ee23007082fee9183c27476d7
GuanKr <guanqikrxern@163.com>
2019-07-02 12:04:18 +0800

    1

--- src/main/java/neu/his/interceptor/LoginInterceptor.java
+++ src/main/java/neu/his/interceptor/LoginInterceptor.java
@@ -12,7 +12,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
-        //获取请求的RUi:去除http:localhost:8080这部分剩下的
+/*        //获取请求的RUi:去除http:localhost:8080这部分剩下的
         String uri = request.getRequestURI();
         System.out.println(uri);
         //UTL:除了login.jsp是可以公开访问的，其他的URL都进行拦截控制
@@ -29,8 +29,8 @@ public class LoginInterceptor implements HandlerInterceptor {
         //不符合条件的给出提示信息，并转发到登录页面
         request.setAttribute("msg", "您还没有登录，请先登录！");
         request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
-        return false;
-//        return  true;
+        return false;*/
+        return  true;
     }
 
     @Override
